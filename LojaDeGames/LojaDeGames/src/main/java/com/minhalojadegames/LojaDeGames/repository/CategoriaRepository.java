a850916b19229c95ede17bb05a2b71a2b4fcd805
aline-brilhante <lykarms@gmail.com>
2021-06-01 18:50:20 -0300

    exercício

---
+++ LojaDeGames/LojaDeGames/src/main/java/com/minhalojadegames/LojaDeGames/repository/CategoriaRepository.java
@@ -0,0 +1,12 @@
+package com.minhalojadegames.LojaDeGames.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.minhalojadegames.LojaDeGames.model.Categoria;
+
+public interface CategoriaRepository extends JpaRepository<Categoria, Long> {
+
+	public List<Categoria> findAllByNomeContainingIgnoreCase(String nome);
+}
